cef45ad7f7df57d00cb16ff19f48f80db2525ab6
M.Usama <usamazarishah@gmail.com>
2024-12-27 20:48:25 +0500

    First-Assign-Backend-done

---
+++ index.mjs
@@ -0,0 +1,21 @@
+import express from 'express'
+import { products } from './productData.mjs'
+import cors from "cors"
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/', (req, response) => {
+  response.send({name:"usama", age:18})
+})
+
+app.get('/products', (req, response) => {
+  // response.send({name:"kiwi", price:"800", quantity:"1kg"})
+  response.send(products)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
